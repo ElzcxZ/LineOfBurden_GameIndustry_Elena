887a19ed6917f3ffba06c0d0debfbfd814e275f2
ElzcxZ <elena.micallef.i79471@mcast.edu.mt>
2026-03-19 10:31:23 +0100

    Added weight mechanic + cooldown to player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -11,14 +11,17 @@ public class Player : MonoBehaviour
     public int maxLane = 2; // actual number of lanes = maxLane + 1
     public float laneSwitchSpeed = 1.5f;
 
-    public bool isMoving = false;
-    public float weight = 1f; // 1 = normal weight, 2 = double weight, (minimum 1)
+    private bool isMoving = false;
     private float newX;
-
-    void Start()
-    {
-        Debug.Log(currentLane.ToString());
-    }
+    
+    public float weight = 1f; // 1 = normal weight, 2 = double weight, (minimum 1)
+    private float minWeight = 1f;
+    public float maxWeight = 3f;
+    public float weightIncreaseSpeed = 0.05f;
+    
+    public float cooldown = 3f;
+    public float lastResetTime = 0f;
+    
     
     // Update is called once per frame
     void Update()
@@ -27,24 +30,30 @@ public class Player : MonoBehaviour
         if (Keyboard.current.wKey.wasPressedThisFrame)
         {
             isMoving = true;
-            Debug.Log(currentLane.ToString());
         }
         if (isMoving)
         {
-            transform.Translate(Vector3.forward * movementSpeed * Time.deltaTime); 
+            weight += weightIncreaseSpeed * Time.deltaTime; // increase weight over time
+            weight = Mathf.Clamp(weight, 1f, maxWeight);
+            transform.Translate(Vector3.forward * (movementSpeed / weight) * Time.deltaTime); 
         }
         
         // if "A" is pressed, switch to the next left lane
         if (Keyboard.current.aKey.wasPressedThisFrame)
         {
             currentLane = Mathf.Clamp(currentLane - 1, 0, maxLane); // "Mathf.Clamp" locks the first value between the 2nd value and the 3rd value
-            Debug.Log(currentLane.ToString());
         }
         // if "D" is pressed, switch to the next right lane
         if (Keyboard.current.dKey.wasPressedThisFrame)
         {
             currentLane = Mathf.Clamp(currentLane + 1, 0, maxLane);
-            Debug.Log(currentLane.ToString());
+        }
+
+        // if "F" is pressed, and if current time is greater than the last time cooldown was reset + cooldown, resets weight
+        if (Keyboard.current.fKey.wasPressedThisFrame && Time.time >= lastResetTime + cooldown)
+        {
+            weight = minWeight;
+            lastResetTime = Time.time; // sets variable to current time
         }
         
         // smooth movement to lane
@@ -53,5 +62,3 @@ public class Player : MonoBehaviour
         transform.position = new Vector3(newX, transform.position.y, transform.position.z);
     }
 }
-
-
