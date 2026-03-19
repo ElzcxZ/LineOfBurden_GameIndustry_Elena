8287bc81a3c26714e346cd7ca6a586a78695735c
ElzcxZ <elena.micallef.i79471@mcast.edu.mt>
2026-03-19 09:44:33 +0100

    Fixed player lane system

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,14 +7,12 @@ public class Player : MonoBehaviour
 { 
     public float movementSpeed = 5.5f;
     
-    public int currentLane = 0; // 1 = left, 2 = middle, 3 = right
+    public int currentLane = 1; // 0 = left, 1 = middle, 2 = right
     public int maxLane = 2; // actual number of lanes = maxLane + 1
-    public float laneDistance = 3f;
     public float laneSwitchSpeed = 1.5f;
 
     public bool isMoving = false;
     public float weight = 1f; // 1 = normal weight, 2 = double weight, (minimum 1)
-    private float targetX;
     private float newX;
 
     void Start()
@@ -39,20 +37,18 @@ public class Player : MonoBehaviour
         // if "A" is pressed, switch to the next left lane
         if (Keyboard.current.aKey.wasPressedThisFrame)
         {
-            currentLane = Mathf.Clamp(currentLane - 1, 1, maxLane); // "Mathf.Clamp" locks the first value between the 2nd value and the 3rd value
+            currentLane = Mathf.Clamp(currentLane - 1, 0, maxLane); // "Mathf.Clamp" locks the first value between the 2nd value and the 3rd value
             Debug.Log(currentLane.ToString());
         }
         // if "D" is pressed, switch to the next right lane
         if (Keyboard.current.dKey.wasPressedThisFrame)
         {
-            currentLane = Mathf.Clamp(currentLane + 1, 1, maxLane);
+            currentLane = Mathf.Clamp(currentLane + 1, 0, maxLane);
             Debug.Log(currentLane.ToString());
         }
         
-        // calculate target X location
-        targetX = currentLane;
         // smooth movement to lane
-        newX = Mathf.Lerp(transform.position.x, targetX, laneSwitchSpeed * Time.deltaTime / weight); // "Mathf.Lerp" linearly transitions between 1st value and 2nd value, with the speed of the 3rd value
+        newX = Mathf.Lerp(transform.position.x, currentLane, laneSwitchSpeed * Time.deltaTime / weight); // "Mathf.Lerp" linearly transitions between 1st value and 2nd value, with the speed of the 3rd value
         
         transform.position = new Vector3(newX, transform.position.y, transform.position.z);
     }
