132767d60251e79a4840455de4321d599e0306ae
ElzcxZ <elena.micallef.i79471@mcast.edu.mt>
2026-03-20 15:11:47 +0100

    Changed lain system

---
+++ Assets/Scripts/Cart.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Cart : MonoBehaviour
+{
+    public float movementSpeed = 5.5f;
+    
+    private bool isMoving = false;
+    
+    public float weight = 1f; // 1 = normal weight, 2 = double weight, (minimum 1)
+    private float minWeight = 1f;
+    public float maxWeight = 3f;
+    public float weightIncreaseSpeed = 0.05f;
+
+    public float cooldown = 3f;
+    public float lastResetTime = 0f;
+
+    void Update()
+    {
+        // player constant movement 
+        if (Keyboard.current.wKey.wasPressedThisFrame)
+        {
+            isMoving = true;
+        }
+        
+        if (isMoving)
+        {
+            weight += weightIncreaseSpeed * Time.deltaTime; // increase weight over time
+            weight = Mathf.Clamp(weight, 1f, maxWeight);
+            transform.Translate(Vector3.forward * (movementSpeed / weight) * Time.deltaTime); 
+        }
+
+        // if "F" is pressed, and if current time is greater than the last time cooldown was reset + cooldown, resets weight
+        if (Keyboard.current.fKey.wasPressedThisFrame && Time.time >= lastResetTime + cooldown)
+        {
+            weight = minWeight;
+            lastResetTime = Time.time; // sets variable to current time
+        }
+    }
+        
+}
---
+++ Assets/Scripts/LaneChanger.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class LaneChanger : MonoBehaviour
+{
+    public Transform[] lanes;
+    
+    public int currentLane = 1; // 0 = left, 1 = middle, 2 = right
+    public float laneSwitchSpeed = 1.5f;
+
+    private void Update()
+    {
+        // if "A" is pressed, switch to the next left lane
+        if (Keyboard.current.aKey.wasPressedThisFrame)
+        {
+            currentLane = Mathf.Max(currentLane - 1, 0); // "Mathf.Clamp" locks the first value between the 2nd value and the 3rd value
+        }
+        // if "D" is pressed, switch to the next right lane
+        if (Keyboard.current.dKey.wasPressedThisFrame)
+        {
+            currentLane = Mathf.Min(currentLane + 1, lanes.Length - 1);
+        }
+        
+        Vector3 nextPosition = Vector3.MoveTowards(transform.localPosition, lanes[currentLane].localPosition, laneSwitchSpeed * Time.deltaTime);
+        transform.localPosition = nextPosition;
+    }
+}
