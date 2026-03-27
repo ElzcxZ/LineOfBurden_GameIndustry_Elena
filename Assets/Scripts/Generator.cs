3b72a7a163ec43af903903f9e2a87c920dfcf0d2
ElzcxZ <elena.micallef.i79471@mcast.edu.mt>
2026-03-27 15:05:20 +0100

    added randomized obstacle mechanic

---
+++ Assets/Scripts/Generator.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class Generator : MonoBehaviour
+{
+    public GameObject itemToSpawn;
+    public Transform[] lanes;
+    public float distance = 5f;
+    public int iterations = 100;
+
+    private void Start()
+    {
+        for (int i = 0; i < iterations; i++)
+        {
+            int idx = Random.Range(0, lanes.Length);
+            Transform lane = lanes[idx];
+            
+            Instantiate(itemToSpawn, lane.position, lane.rotation);
+            transform.position += Vector3.forward  * distance;
+        }
+    }
+}
--- Assets/Scripts/LaneChanger.cs
+++ Assets/Scripts/LaneChanger.cs
@@ -4,6 +4,7 @@ using UnityEngine.InputSystem;
 
 public class LaneChanger : MonoBehaviour
 {
+    public Rigidbody myRigidbody;
     public Transform[] lanes;
     
     public int currentLane = 1; // 0 = left, 1 = middle, 2 = right
@@ -14,15 +15,24 @@ public class LaneChanger : MonoBehaviour
         // if "A" is pressed, switch to the next left lane
         if (Keyboard.current.aKey.wasPressedThisFrame)
         {
-            currentLane = Mathf.Max(currentLane - 1, 0); // "Mathf.Clamp" locks the first value between the 2nd value and the 3rd value
+            currentLane =
+                Mathf.Max(currentLane - 1,
+                    0); // "Mathf.Clamp" locks the first value between the 2nd value and the 3rd value
         }
+
         // if "D" is pressed, switch to the next right lane
         if (Keyboard.current.dKey.wasPressedThisFrame)
         {
             currentLane = Mathf.Min(currentLane + 1, lanes.Length - 1);
         }
-        
-        Vector3 nextPosition = Vector3.MoveTowards(transform.localPosition, lanes[currentLane].localPosition, laneSwitchSpeed * Time.deltaTime);
-        transform.localPosition = nextPosition;
+    }
+    
+    void FixedUpdate()
+    {
+        Vector3 nextPosition = Vector3.MoveTowards(transform.position, lanes[currentLane].position, laneSwitchSpeed * Time.fixedDeltaTime);
+        nextPosition.y = transform.position.y;
+        nextPosition.z = lanes[currentLane].position.z;
+
+        myRigidbody.MovePosition(nextPosition);
     }
 }
